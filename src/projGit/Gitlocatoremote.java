46b44558b1190f057d176c068a1257ad01a3e871
Vinothramadoss <vinoth6161@gmail.com>
2019-01-24 23:37:24 +0530

    created baseclass

---
+++ src/projGit/Gitlocatoremote.java
@@ -0,0 +1,11 @@
+package projGit;
+
+public class Gitlocatoremote {
+private static void vinoth() {
+	// TODO Auto-generated method stub
+	System.out.println("welcome");
+}
+public static void main(String[] args) {
+	vinoth();
+}
+}
